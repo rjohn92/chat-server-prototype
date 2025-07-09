4e9e7772e1bca6910d4e13f8dde3816ecd2bc794
rjohn92 <rolandjohn92@gmail.com>
2025-07-09 00:06:17 -0400

    Initial commit

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+HOST = '127.0.0.1'
+PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))            # 1. Connect to server
+    msg = input("Enter message: ")     # 2. Get user input
+    s.sendall(msg.encode())            # 3. Send message
+    data = s.recv(1024)                # 4. Receive echo
+    print("Server replied:", data.decode())
---
+++ server.py
@@ -0,0 +1,16 @@
+import socket  # [Sockets, Message Handling]
+
+HOST = '127.0.0.1'  # [Protocol/Workflow: where to listen]
+PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # [Sockets]
+    s.bind((HOST, PORT))    	# [Listener: binds to address/port]
+    s.listen(1)             	# [Listener: ready for 1 connection]
+    print("Server listening on", (HOST, PORT))
+    conn, addr = s.accept() # [Listener: accept a client]
+    with conn:
+        print("Client connected:", addr)  # [Testing/Interaction]
+        data = conn.recv(1024)            # [Message Handling: receive bytes]
+        print("Received:", data.decode())  # [Message Handling: decode text]
+        conn.sendall(data)                 # [Protocol: echo back]
+        # [Shutdown: 'with' auto-closes sockets]
